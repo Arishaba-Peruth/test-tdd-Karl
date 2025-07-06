65c2b7c56a6f4cc230909fe7b70a8435874ebde7
Arishaba-Peruth <lorisperutha@gmail.com>
2025-07-06 22:08:54 +0300

    here the * operator handles the case no need to create another function in multiply.py

--- test_multiply.py
+++ test_multiply.py
@@ -4,7 +4,7 @@ from multiply import multiply
 #   assert multiply(1,1) == 1
 
 #Test 2
-def test_multiply_one_by_one():
+"""def test_multiply_one_by_one():
     assert multiply(1,1) == 1
 
 def test_multiply_two_by_two():
@@ -18,4 +18,17 @@ def test_multiply_two_by_two():
     assert multiply(2,2) == 4
 
 def test_multiply_three_by_three():
-    assert multiply(3,3) == 9
+    assert multiply(3,3) == 9"""
+
+#Test 4
+def test_multiply_one_by_one():
+    assert multiply(1, 1) == 1
+
+def test_multiply_two_by_two():
+    assert multiply(2, 2) == 4
+
+def test_multiply_three_by_three():
+    assert multiply(3, 3) == 9
+
+def test_multiply_four_by_four():
+    assert multiply(4, 4) == 16
