08901b8434890511473d114441b99c65ced0325b
Arishaba-Peruth <lorisperutha@gmail.com>
2025-07-06 22:04:10 +0300

    3 tests passed now

--- multiply.py
+++ multiply.py
@@ -2,9 +2,13 @@
 #    return 1
 
 # This is the implementation of the multiply function that will pass the tests defined in test_multiply.py
-def multiply(a,b):
+"""def multiply(a,b):
     if a == 1 and b ==1:
         return 1
     if a ==2 and b == 2:
         return 4
-    return 0
+    return 0"""
+
+# The function is designed to pass the specific tests provided in the test_multiply.py file.
+def multiply(a, b):
+    return a * b
