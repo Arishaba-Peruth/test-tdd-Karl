998faa20c32f3382678fff34b36b1c5ebef1073f
Arishaba-Peruth <lorisperutha@gmail.com>
2025-07-06 22:11:49 +0300

    added test 5 to handle large numbers

--- test_multiply.py
+++ test_multiply.py
@@ -21,6 +21,19 @@ def test_multiply_three_by_three():
     assert multiply(3,3) == 9"""
 
 #Test 4
+"""def test_multiply_one_by_one():
+    assert multiply(1, 1) == 1
+
+def test_multiply_two_by_two():
+    assert multiply(2, 2) == 4
+
+def test_multiply_three_by_three():
+    assert multiply(3, 3) == 9
+
+def test_multiply_four_by_four():
+    assert multiply(4, 4) == 16"""
+
+#Test 5
 def test_multiply_one_by_one():
     assert multiply(1, 1) == 1
 
@@ -32,3 +45,6 @@ def test_multiply_three_by_three():
 
 def test_multiply_four_by_four():
     assert multiply(4, 4) == 16
+
+def test_multiply_large_number():
+    assert multiply(50, 90) == 50 * 90
