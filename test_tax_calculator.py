62beda5428819d851a3bd4877032c58d35d8d1c5
Arishaba-Peruth <lorisperutha@gmail.com>
2025-07-10 10:51:12 +0300

    renamed my functions in test_tax_calculator.py

--- test_tax_calculator.py
+++ test_tax_calculator.py
@@ -5,56 +5,56 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from tax_calculator import calculate_tax
 
-def test_tax_for_zero_earnings():
+def test_tax_A():   #for_zero_earnings
     """
     Test case: Earnings are 0.
     Expected: Tax should be 0.
     """
     assert calculate_tax(0) == 0
 
-def test_tax_for_earnings_below_first_bracket():
+def test_tax_B():   #for_earnings_below_first_bracket
     """
     Test case: Earnings are 11999 (just below the 12000 threshold).
     Expected: Tax should be 0.
     """
     assert calculate_tax(11999) == 0
 
-def test_tax_for_earnings_at_first_bracket_threshold():
+def test_tax_C():   #for_earnings_at_first_bracket_threshold
     """
     Test case: Earnings are exactly 12000.
     Expected: Tax should be 0 (as rule is '< 12000').
     """
     assert calculate_tax(12000) == 0
 
-def test_tax_for_earnings_just_above_first_bracket():
+def test_tax_D():   #for_earnings_just_above_first_bracket
     """
     Test case: Earnings are 12001.
     Expected: Tax should be 20% of (12001 - 12000) = 20% of 1 = 0.20.
     """
     assert calculate_tax(12001) == 0.20
 
-def test_tax_for_mid_range_second_bracket():
+def test_tax_E():   #for_mid_range_second_bracket
     """
     Test case: Earnings are 20000.
     Expected: Tax should be 20% of (20000 - 12000) = 20% of 8000 = 1600.
     """
     assert calculate_tax(20000) == 1600
 
-def test_tax_for_earnings_just_below_third_bracket():
+def test_tax_F():   #for_earnings_just_below_third_bracket
     """
     Test case: Earnings are 35999.
     Expected: Tax should be 20% of (35999 - 12000) = 20% of 23999 = 4799.80.
     """
     assert calculate_tax(35999) == 4799.80
 
-def test_tax_for_earnings_at_second_bracket_threshold():
+def test_tax_G(): #for_earnings_at_second_bracket_threshold
     """
     Test case: Earnings are exactly 36000.
     Expected: Tax should be 20% of (36000 - 12000) = 20% of 24000 = 4800.
     """
     assert calculate_tax(36000) == 4800
 
-def test_tax_for_earnings_just_above_second_bracket():
+def test_tax_H():   #for_earnings_just_above_second_bracket
     """
     Test case: Earnings are 36001.
     Expected:
@@ -64,7 +64,7 @@ def test_tax_for_earnings_just_above_second_bracket():
     """
     assert calculate_tax(36001) == 4800.40
 
-def test_tax_for_high_earnings_third_bracket():
+def test_tax_I():   #for_high_earnings_third_bracket
     """
     Test case: Earnings are 50000.
     Expected:
